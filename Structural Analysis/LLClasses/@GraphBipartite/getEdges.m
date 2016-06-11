3d5ff87db0b991c950399dd86b75202ec31e2d9e
George ZP <tailwhipx4@gmail.com>
2016-06-11 12:48:15 +0300

    Fixed bug in getEdges
    It would not generate the edgelist properly in already matched graphs

--- Structural Analysis/LLClasses/@GraphBipartite/getEdges.m
+++ Structural Analysis/LLClasses/@GraphBipartite/getEdges.m
@@ -20,24 +20,37 @@ end
 
 E = [];
 for i=1:gh.numEdges
+    if debug fprintf('Examining edge with ID: %d ',gh.edges(i).id); end
     flagE2V = true;
     flagV2E = true;
+    equId = gh.edges(i).equId;
+    varId = gh.edges(i).varId;
     varIndex = gh.getIndexById(gh.edges(i).varId);
     if debug fprintf('linking equtation %d and variable %d\n',gh.edges(i).equId, gh.edges(i).varId); end
+
     if gh.variables(varIndex).isKnown
         % No operation
     end
     if gh.variables(varIndex).isMeasured
         flagE2V = false;
+        if debug fprintf('The E->V direction is disabled, because the variable is measured\n'); end
     end
     if gh.variables(varIndex).isInput
         flagE2V = false; % From equation to variable
+        if debug fprintf('The E->V direction is disabled, because the variable is an input\n'); end
     end
     if gh.variables(varIndex).isOutput
         % No operation
     end
-    if ~gh.isMatchable(gh.edges(i).id)
+    if gh.edges(i).isMatched
+        flagV2E = false;
+        if debug fprintf('The V->E direction is disabled, because the edge is matched\n'); end
+    elseif gh.isMatched(varId)
+        flagE2V = false;
+        if debug fprintf('The E->V direction is disabled, because the variable is matched\n'); end
+    elseif ~gh.isMatchable(gh.edges(i).id)
         flagE2V = false; % Equation to Variable
+        if debug fprintf('The E->V direction is disabled, because the variable cannot be matched\n'); end
     end
     if flagE2V && ~noE2V
         E(end+1,:) = [gh.edges(i).equId gh.edges(i).varId gh.edges(i).weight]; % with added cost of solving the edge
