5606ed1a23f34727a4c167815d872b09a8aca670
Georacer <tailwhipx4@gmail.com>
2017-01-04 16:20:03 +0200

    Deleted deprecated/unnecessary files

--- Structural Analysis/LLCreation/addResidual.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = addResidual( input_args )
-%ADDRESIDUAL Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- Structural Analysis/LLCreation/getEdges.m
+++
@@ -1,9 +0,0 @@
-function [ rows, cols ] = getEdges( Graph )
-%GETEDGES Summary of this function goes here
-%   Detailed explanation goes here
-[rows, cols] = find(Graph.adjacency);
-[rows, I] = sort(rows);
-cols = cols(I);
-
-end
-
